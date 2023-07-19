3986c85bf15ca4194f279456dccdd96b6feb3df0
https://github.com/msurio <suriomanthony@gmail.com>
2023-07-19 11:31:47 -0700

    Values and Variables

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,28 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+//Values & Varables
+//values example:
+console.log('Jonas');
+console.log(23);
+
+// variable example:
+let firstName = 'Jonas';
+console.log(firstName)
+
+//good example of variable names
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+//bad example of variable names
+let job1 = 'Programmer';
+let job2 = 'Teacher';
+
+//Practice Value and Variables
+let country = 'United States';
+let continent = 'North America';
+let population = 10000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
