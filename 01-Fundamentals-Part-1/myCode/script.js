fa87a5df0c7cb4cc25334ce0fcdd32f8539d9976
Marc Anthony Surio <suriomanthony@gmail.com>
2023-07-21 18:55:34 -0700

    else/if statement practice

--- 01-Fundamentals-Part-1/myCode/script.js
+++ 01-Fundamentals-Part-1/myCode/script.js
@@ -291,3 +291,51 @@ console.log(description);
 */
 //////////////////////////////////////////////////////////////////////////////////////////////
 
+//Taking Decisions: If/Else Statements:
+/*
+const age = 15;
+
+if(age >= 18){
+    console.log('Sarah can start driving license 😃');
+    //windows + . creates the emoji popup window
+}else{
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years 😢`);
+}
+
+const birthYear = 1996;
+let century;
+
+if(birthYear <= 2000){
+     century = 20;
+}else{
+     century = 21;
+}
+
+console.log(century);
+
+//If/Else Statement practice:
+
+1. If your country's population is greater that 33 million, log a string like this to the
+console: 'Portugal's population is above average'. Otherwise, log a string like
+'Portugal's population is 22 million below average' (the 22 is the average of 33
+minus the country's population)
+
+2. After checking the result, change the population temporarily to 13 and then to
+130. See the different results, and set the population back to original
+
+
+// const population = 11;
+const population = 13;
+// const population = 130;
+
+if (population > 33){
+    console.log("Porutal population is above average")
+}else{
+    const averagePopulation = 33
+    console.log(`Portugal's population is ${averagePopulation - population} million below average`);
+}
+*/
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+//Coding Challenge #2:
